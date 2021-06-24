336495a97e37ef1dc69bcc3b614baa6601857e18
Squire <esq.berrouz@gmail.com>
2021-06-24 21:38:07 +0500

    Сделал JS для формы и гамбургер-меню

--- build/js/script.js
+++ build/js/script.js
@@ -1,15 +1,39 @@
-'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+// 'use strict';
+// var pageHeader = document.querySelector('.page-header');
+// var headerToggle = document.querySelector('.page-header__toggle');
 
-pageHeader.classList.remove('page-header--nojs');
+// pageHeader.classList.remove('page-header--nojs');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
+// headerToggle.addEventListener('click', function () {
+//   if (pageHeader.classList.contains('page-header--closed')) {
+//     pageHeader.classList.remove('page-header--closed');
+//     pageHeader.classList.add('page-header--opened');
+//   } else {
+//     pageHeader.classList.add('page-header--closed');
+//     pageHeader.classList.remove('page-header--opened');
+//   }
+// });
+let navButton = document.querySelector('.nav__button');
+let navList = document.querySelector('.nav__list');
+let pageBody = document.querySelector('.page__body');
+let inputTel = document.querySelector('input[type="tel"]')
+
+navButton.classList.remove('nav__button--without-js');
+navList.classList.remove('nav-__list--without-js');
+document.querySelector('.header').classList.remove('header--without-js');
+document.querySelector('.header__logo').classList.remove('header__logo--without-js');
+
+inputTel.addEventListener('input', () => {
+  inputTel.value = inputTel.value.replace(/[^\d]/g, '');
+})
+
+navButton.addEventListener('click', () => {
+  navButton.classList.toggle('nav__button--open');
+  navList.classList.toggle('nav__list--open');
+
+  if (navList.classList.contains('nav__list--open')) {
+    pageBody.classList.add('page__body--overflow-hidden');
   } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
+    pageBody.classList.remove('page__body--overflow-hidden');
   }
-});
+})
